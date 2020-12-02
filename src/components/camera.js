1b7135f30af2fe554f76e2d851f03c0469b3a832
qipanyang <qipanyang@126.com>
2020-12-02 15:33:50 -0600

    seperate take photo and save

--- src/components/camera.js
+++ src/components/camera.js
@@ -9,7 +9,9 @@ import {
   Button,
   TouchableWithoutFeedback,
   Dimensions,
+  CameraRoll
 } from 'react-native';
+
 // import CameraRoll, {saveToCameraRoll} from '@react-native-community/cameraroll';
 
 class MyCamera extends React.Component {
@@ -18,23 +20,25 @@ class MyCamera extends React.Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    console.log("rerendering camera.js");
+    console.log('rerendering camera.js');
     if (this.props.takePhoto === true) {
-      console.log("update taking picutre.");
+      console.log('update taking picutre.');
       // this.takePicture();
       // this.props.switchTakePhoto(false);
+      this.takePicture.bind(this)();
+      this.props.switchTakePhoto(false);
+    } else {
+      console.log('not updating taking picture');
     }
-    else {
-      console.log("not updating taking picture");
-    }
-    this.takePicture.bind(this)();
+
   }
   takePicture = async () => {
     // if (this.camera && !this.props.takePhoto) {
-      const options = {quality: 0.5, base64: true};
-      const data = await this.camera.takePictureAsync(options);
-      console.log('taking a picture', this.props.cameratype);
-      console.log(this.props.cameratype, this.props.flash, this.props.exposure);
+    const options = {quality: 0.5, base64: true};
+    const data = await this.camera.takePictureAsync(options);
+    // CameraRoll.saveToCameraRoll(data.uri, 'photo');
+    console.log('taking a picture');
+    console.log(data.base64);
     // }
   };
   render() {
@@ -53,14 +57,17 @@ class MyCamera extends React.Component {
           <Grid style={styles.bottomToolbar}>
             <Row>
               <Col size={2}>
-                <Button title="switch" onPress={() => this.props.switchOccupy(0)} />
+                <Button
+                  title="switch"
+                  onPress={() => this.props.switchOccupy(0)}
+                />
               </Col>
               <Col size={2}>
                 <TouchableOpacity>
                   <Button
-                      style={styles.captureBtn}
-                      title={'Snap'}
-                      onPress={this.takePicture.bind(this)}
+                    style={styles.captureBtn}
+                    title={'Snap'}
+                    onPress={this.takePicture.bind(this)}
                   />
                 </TouchableOpacity>
               </Col>
@@ -70,7 +77,6 @@ class MyCamera extends React.Component {
       </View>
     );
   }
-
 }
 
 const {width: winWidth, height: winHeight} = Dimensions.get('window');
